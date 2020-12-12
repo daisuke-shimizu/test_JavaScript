4bbfff884584660d1509af3810ae8c7cabc1d839
daisuke-shimizu <vitamin.daisuke.0901@gmail.com>
2020-12-12 19:31:54 +0900

    test_saiten

---
+++ test_saiten.js
@@ -0,0 +1,23 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  let times = Number(lines[0].split(' ')[0]);
+  let mark = lines[0].split(' ')[1];
+  for(let i = 1; i <= times; i++){
+      let score = Number(lines[i].split(' ')[0])- (Number(lines[i].split(' ')[1])*5);
+      if(score <0){
+          score = 0;
+      }if(score>= mark){
+      console.log(i);
+      }
+  }
+});
